05bb06695c0e529957481e050f86bd4eb61abdf3
Guilherme <57897031+guilherme-batalheiro@users.noreply.github.com>
2022-05-10 00:22:37 +0100

    Delete updateSongWorker.js

--- web-app/updateSongWorker.js
+++
@@ -1,9 +0,0 @@
-function updateSong(room_code) {
-    setTimeout(() => { }, 5000)
-    postMessage(room_code)
-}
-
-self.addEventListener("message", function(e) {
-    room_code = e.data
-    updateSong(room_code)
-}, false);
